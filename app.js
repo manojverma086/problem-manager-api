4be1549ab2515ccbaa81b408045883a621b35ffa
manoj <verma.manoj241@gmail.com>
2019-08-05 23:30:46 +0530

    Update app.js
    Moved cors above

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,7 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/problems', problemRouter);
-app.use(cors());
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
